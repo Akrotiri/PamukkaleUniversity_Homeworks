3374f41ff8b0834dffca120405de30d5a0df0317
battiness <fulya_akcan@hotmail.com>
2013-10-13 01:18:02 +0300

    Code comments added

--- FulyaBank/banka/main.c
+++ FulyaBank/banka/main.c
@@ -14,6 +14,8 @@
 
 #define NEWLINE "\n"
 #define True 1
+// Dizi boyutunu hesaplayan macro. Bellekteki dizi boyutunu 
+// ilk dizi elemani boyutuna bolerek dizi elemani sayisini buluruz
 #define SizeOfArray(x)  (sizeof(x) / sizeof(x[0]))
 
 float para_miktari[] = {500.00,400.23,230.65};
@@ -21,6 +23,8 @@ char* hesap_no[] = {"100.80.23","100.80.25","100.80.28"};
 char* username[] = {"fulya","recep","kadir"};
 char* password[] = {"akcan","erol","yaldir"};
 
+// Bu fonksiyonda secim yapilan hesabin oturum acma bilgileri
+// username ve password degiskenlerinin kontroluyle saglanir.
 int login(int choice) {
 	char *input_username, *input_password;
 	int valid;
@@ -31,7 +35,9 @@ int login(int choice) {
 	printf("Sifrenizi giriniz: ");
 	scanf("%s",input_password);
 
-	if(strcmp(input_username,username[choice]) == 0 && strcmp(input_password,password[choice]) == 0){
+	if(strcmp(input_username,username[choice]) == 0 && 
+		strcmp(input_password,password[choice]) == 0){
+
 		printf("Girdiginiz bilgiler dogru\n");
 		valid = 1;
 	}
@@ -45,17 +51,20 @@ int login(int choice) {
 	return valid;
 }
 
-
+// Secimi yapilmis hesabin bilgileri gosterilir
 void hesapGoster(int hesap) {
 	printf("Hesap no: %s\n",hesap_no[hesap]);
 	printf("Mevduat: %.2f\n",para_miktari[hesap]);
 }
 
+// Oturum acilmis ya da havale yapilacak hesaba
+// girilen miktar kadar para eklenir.
 int paraEkle(int hesap, float miktar) {	
 	para_miktari[hesap] = para_miktari[hesap] + miktar;
 	return 1;
 }
 
+// Oturum acilmis hesaptan girilen miktar kadar para cekilir.
 int paraCek(int hesap, float miktar) {
 	int valid;
 	if (miktar > para_miktari[hesap]){
@@ -69,12 +78,15 @@ int paraCek(int hesap, float miktar) {
 	return valid;
 }
 
+// Oturum acilmis hesaptan havale yapilacak hesaba 
+// girilen miktar kadar havale islemi yapilir.
 void havale(int hesap1,int hesap2,float miktar) {
 	if(paraCek(hesap1, miktar)){
 		paraEkle(hesap2, miktar);
 	}
 }
 
+// Kullaniciya secim yapabilecegi menu sunulur
 int menu() {
 	int choice = 1;	
 	printf(NEWLINE);
@@ -90,6 +102,9 @@ int menu() {
 	return choice;
 }
 
+// Verilen hesap numarasindan bulundugu 
+// dizindeki indeks numarasi hesaplanir, 
+// bulunamazsa bulunamadigini belirten deger olarak -1 donulur
 int hesapBul(char* hesapNo) {
 	int index;
 	int arraySize = SizeOfArray(hesap_no);
@@ -105,8 +120,11 @@ int main(int argc, char *argv[]) {
 	float miktar = 0;
 	int hesap_no1_index, hesap_no2_index;
 	char *havale_no;
+	// havale_no icin bellekte 50 karakterlik yer acilir
 	havale_no = (char*) malloc(sizeof(char) * 50);
 
+	// Programda yer alan hesap numaralarindan birisi girilinceye kadar
+	// kullaniciya hesap numarasi sorulur
 	do {
 		printf("Islem yapilacak hesap numarasini giriniz: ");
 		scanf("%s", havale_no);
@@ -116,6 +134,7 @@ int main(int argc, char *argv[]) {
 		}
 	} while(hesap_no1_index == -1);
 	
+	// Oturum acilana kadar kullaniciya kullanici adi ve sifre sorulur
 	while(login(hesap_no1_index) != 1);
 
 	do{
@@ -148,13 +167,16 @@ int main(int argc, char *argv[]) {
 					printf("Havale edilecek hesap numarasi bulunamadi!" NEWLINE);
 				}				
 				break;
+			// Varsayilan 4 menu disinda bir secim yapildiginda programdan cikilir
 			case 5:
 			default:
+				// Bellek bosaltiliyor, programdan cikiliyor
 				free(havale_no);
 				return 0;
 		}
 	} while(True);
 
+	// Bellek bosaltiliyor, programdan cikiliyor
 	free(havale_no);
 	return 0;
 }
